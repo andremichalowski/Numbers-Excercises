1878b64dda31a424e087b8942d88d9debaabcc2d
amichalo421 <andre.michalowski@gmail.com>
2019-10-28 22:40:18 +0100

    Prompt-2.1-Solution

--- index-2.js
+++ index-2.js
@@ -2,6 +2,9 @@
 // taking the first 3 characters and the last 3 characters of a string and adding them together. 
 // The string length must be 3 or SVGPathSegMovetoRel, if not, the original string is returned.Js
 
+const makeNewString = (str) => 
+    str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+
 
 
 
