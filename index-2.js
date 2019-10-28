ec8a6b0e62fa85134856b85cc159b5e8df1a9eb8
amichalo421 <andre.michalowski@gmail.com>
2019-10-28 22:41:44 +0100

    Prompt-2.1-Tests

--- index-2.js
+++ index-2.js
@@ -5,7 +5,11 @@
 const makeNewString = (str) => 
     str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
 
+console.log(makeNewString('abc'));
+console.log(makeNewString('abcdef'));
+console.log(makeNewString('abc123abc123'));
 
+console.log(makeNewString('ab'));
 
 
 
