9d4607e73456fb3a6d273620ebdc837b00c41b10
amichalo421 <andre.michalowski@gmail.com>
2019-10-30 22:54:45 +0100

    Prompt-2.5-Test

--- index-2.js
+++ index-2.js
@@ -54,6 +54,12 @@ const concatenate = (str1, str2) =>
 const countChars = (str, char) => 
     str.split('').filter(ch => ch === char ).length;
 
-    const countChars = (str, char) => 
+    const contains2To4 = (str, char) => 
         countChars(str, char) >= 2 && countChars(str, char) <= 4;
+
+console.log(contains2To4('ooh!', 'o'));
+console.log(contains2To4('oh!', 'o'));
+console.log(contains2To4('oooh!', 'o'));
+console.log(contains2To4('ooooh!', 'o'));
+console.log(contains2To4('oooooh!', 'o'));
 // Return Link: https://www.youtube.com/watch?v=U0Ce27lA7R4&feature=youtu.be
